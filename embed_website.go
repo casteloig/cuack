aeb13a3382310301d6ca24cd1779dfae81d20d6c
Ignacio Castelo <nacho.castelo@gmail.com>
2021-09-19 13:45:42 +0200

    Added grateful shutdown of inspect's web

--- embed_website.go
+++ embed_website.go
@@ -1,6 +1,7 @@
 package website
 
 import (
+	"context"
 	"embed"
 	"fmt"
 	"html/template"
@@ -8,6 +9,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	do "cuack/pkg/digitalocean"
 )
@@ -25,55 +28,70 @@ var (
 	}
 )
 
+var i do.Inspect
+
 func CreateWebsite(inspected do.Inspect) error {
 
-	// quit := make(chan os.Signal, 1)
+	i = inspected
 
-	// signal.Notify(quit, os.Interrupt)
+	sm := http.NewServeMux()
+	sm.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(cssFS))))
+	x := http.FileServer(http.FS(content))
+	sm.Handle("/src/html", x)
+	sm.HandleFunc("/", rootHandler)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		page, ok := pages[r.URL.Path]
-		if !ok {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-		tpl, err := template.ParseFS(content, page)
-		if err != nil {
-			log.Println("page not found in pages cache...")
-			w.WriteHeader(http.StatusInternalServerError)
-			log.Println(err)
-			return
-		}
+	server := http.Server{
+		Handler: sm,
+		Addr:    ":8080",
+	}
 
-		w.Header().Set("Content-Type", "text/html")
-		w.WriteHeader(http.StatusOK)
-		if err := tpl.Execute(w, inspected); err != nil {
-			log.Println(err)
-			return
-		}
-	})
-
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(cssFS))))
-
-	http.FileServer(http.FS(content))
 	fmt.Println("You can connect to http://localhost:8080 to see the details")
 
-	go func() {
-		err := http.ListenAndServe(":8080", nil)
-		if err != nil {
-			fmt.Println("Closing server")
-			os.Exit(1)
-		}
-	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	go serv(server)
+
+	<-quit
+	// gracefully stop server
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("server stopped")
+	return nil
+}
+
+func serv(server http.Server) error {
+	err := server.ListenAndServe()
+	if err != nil {
+		return err
+	}
+	fmt.Println("server closed 100")
 
 	return nil
 }
 
-// func gracefullShutdown(server *http.Server, logger *log.Logger, quit <-chan os.Signal) {
-// 	<-quit
-// 	logger.Println("Server is shutting down...")
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	page, ok := pages[r.URL.Path]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	tpl, err := template.ParseFS(content, page)
+	if err != nil {
+		log.Println("page not found in pages cache...")
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
 
-// 	if err := server.Shutdown(ctx); err != nil {
-// 	  logger.Fatalf("Could not gracefully shutdown the server: %v\n", err)
-// 	}
-//   }
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	if err := tpl.Execute(w, i); err != nil {
+		log.Println(err)
+		return
+	}
+}
